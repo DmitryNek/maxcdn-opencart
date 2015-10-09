515021464b9929278001220c91d5dc207d094fd8
DmitryNek <exmail.nek@gmail.com>
2015-10-09 12:47:26 +0300

    Added english language file

---
+++ upload/admin/language/english/module/maxcdn.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @author: Dmitry Shkoliar @DrmitryNek
+ * @project: maxcdn-opencart
+ * @file: admin/language/english/module/maxcdn.php
+ */
+
+// Heading
+$_['heading_title'] = 'MaxCDN';
+
+// Entry
+$_['entry_status'] = 'Status';
+$_['entry_domain'] = 'MaxCDN Domain';
+$_['entry_images'] = 'Deliver Images';
+$_['entry_js'] = 'Deliver JS';
+$_['entry_css'] = 'Deliver CSS';
+
+// Text
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
+$_['text_module'] = 'Modules';
+$_['text_success'] = 'Success: You have modified MaxCDN module!';
+$_['text_edit'] = 'Edit MaxCDN Module';
+
+// Button
+$_['button_save'] = 'Save';
+$_['button_cancel'] = 'Cancel';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify MaxCDN module!';
